20247211385f34c2ae7e1b55e3d1e307bc45fdff
Schlopp <elijahpitts.32@gmail.com>
2022-03-31 20:18:35 -0500

    Tempest GAR complete, Core and UI changes
    textures will change

--- ProjectVanilla/HS_Core/config.cpp
+++ ProjectVanilla/HS_Core/config.cpp
@@ -30,23 +30,43 @@ class cfgFactionClasses
 { 
     class HS_GAR_Fac  //Galatic Republic
     { 
-        displayName = "[HS] Galactic Republic Arsenal"; 
+        displayName = "[HS] Grand Army of the Republic"; 
         priority = 2; // Position in list. 
         side = 1; // Opfor = 0, Blufor = 1, Indep = 2. 
-        icon = ""; //Custom Icon 
+        icon = "\HS_UI\Map\Markers\Flags\rep_ca.paa"; //Custom Icon 
     };
-	class HS_CIS_Fac  //Galatic Republic
+	class HS_CIS_Fac  //Confederacy of Independent Systems
     { 
-        displayName = "[HS] Confederacy Arsenal"; 
+        displayName = "[HS] Confederacy of Independent Systems"; 
         priority = 2; // Position in list. 
         side = 0; // Opfor = 0, Blufor = 1, Indep = 2. 
-        icon = ""; //Custom Icon 
+        icon = "\HS_UI\Map\Markers\Flags\cis_ca.paa"; //Custom Icon 
     };
-	class HS_Indep_Fac  //Galatic Republic
+	class HS_Indep_Fac  //Independent Forces
     { 
-        displayName = "[HS] Independant Arsenal"; 
+        displayName = "[HS] Galactic Insurgents "; 
         priority = 2; // Position in list. 
-        side = 1; // Opfor = 0, Blufor = 1, Indep = 2. 
-        icon = ""; //Custom Icon 
+        side = 2; // Opfor = 0, Blufor = 1, Indep = 2. 
+        icon = "\HS_UI\Map\Markers\Flags\ins_ca.paa"; //Custom Icon 
     };
+};
+
+class CfgEditorSubcategories
+{
+	class HS_CAR
+	{
+		displayName="Cars";
+	};
+	class HS_TANK
+	{
+		displayName="Tanks";
+	};
+	class HS_APC
+	{
+		displayName="Apc";
+	};
+	class HS_Drone
+	{
+		displayName="Drones";
+	};
 };
--- ProjectVanilla/HS_Opfor_Veh/HS_Tempest/config.cpp
+++ ProjectVanilla/HS_Opfor_Veh/HS_Tempest/config.cpp
@@ -32,9 +32,14 @@ class CfgVehicles
     class HS_GAR_Truck_03_transport_F: O_Truck_03_transport_F
     {
 		scope=2;
-		faction="OPF_F";
-		crew="O_Soldier_F";
-		displayName="GAR Temptest Transport Test";
+		side=1;
+		faction="HS_GAR_Fac";
+        editorSubcategory="HS_CAR";
+		crew="B_Soldier_F";
+		displayName="Tempest Transport";
+		model="\A3\Soft_F_EPC\Truck_03\Truck_03_transport_F.p3d";
+		picture="\A3\Soft_F_EPC\Truck_03\Data\UI\truck_03_transport_CA.paa";
+		icon="\A3\Soft_F_EPC\Truck_03\Data\UI\map_Truck_03_Transport_CA.paa";
 		hiddenSelections[]=
 		{
 			"Camo1",
@@ -49,10 +54,10 @@ class CfgVehicles
 		};
 		class TextureSources
 		{
-			class Hex
+			class Standard
 			{
-				displayName="$STR_A3_TEXTURESOURCES_OPFOR0";
-				author="$STR_A3_Bohemia_Interactive";
+				displayName="Republic Standard";
+				author="Helix Studios";
 				textures[]=
 				{
 					"\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_ext01_CO.paa",
@@ -61,13 +66,355 @@ class CfgVehicles
 				};
 				factions[]=
 				{
-					"OPF_F"
+					"HS_GAR_Fac"
+				};
+			};
+			class Padme
+			{
+				displayName="Republic Pinup";
+				author="Helix Studios";
+				textures[]=
+				{
+					"\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_ext01_padme_CO.paa",
+			        "\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_ext02_CO.paa",
+			        "\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_cargo_CO.paa"
+				};
+				factions[]=
+				{
+					"HS_GAR_Fac"
 				};
 			};
 		};
 		textureList[]=
 		{
-			"Hex",
+			"Standard",
+			1,
+			"Padme",
+			1
+		};
+    }
+    class HS_GAR_Truck_03_medevac_F: O_Truck_03_transport_F
+    {
+		scope=2;
+		side=1;
+		faction="HS_GAR_Fac";
+        editorSubcategory="HS_CAR";   
+		crew="B_Soldier_F";
+		displayName="Tempest Transport (Medical)";
+		model="\A3\Soft_F_EPC\Truck_03\Truck_03_covered_F.p3d";
+		picture="\A3\Soft_F_EPC\Truck_03\Data\UI\truck_03_medevac_CA.paa";
+		icon="\A3\Soft_F_EPC\Truck_03\Data\UI\map_Truck_03_medevac_CA.paa";
+		vehicleClass="Support";
+		hiddenSelections[]=
+		{
+			"Camo1",
+			"Camo2",
+			"Camo3",
+			"Camo4"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_ext01_CO.paa",
+			"\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_ext02_CO.paa",
+			"\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_cargo_CO.paa",
+			"\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_medevac_CO.paa"  
+		};
+		class TextureSources
+		{
+			class Standard
+			{
+				displayName="Republic Standard";
+				author="Helix Studios";
+				textures[]=
+				{
+					"\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_ext01_CO.paa",
+			        "\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_ext02_CO.paa",
+			        "\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_cargo_CO.paa",
+                    "\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_medevac_CO.paa"                               
+				};
+				factions[]=
+				{
+					"HS_GAR_Fac"
+				};
+			};
+		};
+		textureList[]=
+		{
+			"Standard",
+			1
+		};
+    }
+	class HS_GAR_Truck_03_covered_F: O_Truck_03_transport_F
+    {
+		scope=2;
+		side=1;
+		faction="HS_GAR_Fac";
+        editorSubcategory="HS_CAR";   
+		crew="B_Soldier_F";
+		displayName="Tempest Transport (Covered)";
+		model="\A3\Soft_F_EPC\Truck_03\Truck_03_covered_F.p3d";
+		picture="\A3\Soft_F_EPC\Truck_03\Data\UI\truck_03_transport_CA.paa";
+		icon="\A3\Soft_F_EPC\Truck_03\Data\UI\map_Truck_03_Transport_CA.paa";
+		hiddenSelections[]=
+		{
+			"Camo1",
+			"Camo2",
+			"Camo3",
+			"Camo4"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_ext01_CO.paa",
+			"\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_ext02_CO.paa",
+			"\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_cargo_CO.paa",
+			"\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_cover_CO.paa"  
+		};
+		class TextureSources
+		{
+			class Standard
+			{
+				displayName="Republic Standard";
+				author="Helix Studios";
+				textures[]=
+				{
+					"\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_ext01_CO.paa",
+			        "\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_ext02_CO.paa",
+			        "\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_cargo_CO.paa",
+					"\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_cover_CO.paa"
+				};
+				factions[]=
+				{
+					"HS_GAR_Fac"
+				};
+			};
+			class Padme
+			{
+				displayName="Republic Pinup";
+				author="Helix Studios";
+				textures[]=
+				{
+					"\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_ext01_padme_CO.paa",
+			        "\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_ext02_CO.paa",
+			        "\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_cargo_CO.paa",
+					"\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_cover_CO.paa" 
+				};
+				factions[]=
+				{
+					"HS_GAR_Fac"
+				};
+			};
+		};
+		textureList[]=
+		{
+			"Standard",
+			1,
+			"Padme",
+			1
+		};
+    }
+	 class HS_GAR_Truck_03_fuel_F: O_Truck_03_transport_F
+    {
+		scope=2;
+		side=1;
+		faction="HS_GAR_Fac";
+        editorSubcategory="HS_CAR";
+		crew="B_Soldier_F";
+		displayName="Tempest Rhydonium Tanker (Fuel)";
+		model="\A3\Soft_F_EPC\Truck_03\Truck_03_fuel_F.p3d";
+		picture="\A3\Soft_F_EPC\Truck_03\Data\UI\truck_03_fuel_CA.paa";
+		icon="\A3\Soft_F_EPC\Truck_03\Data\UI\map_Truck_03_Fuel_CA.paa";
+		vehicleClass="Support";
+		transportFuel=1e+012;
+		supplyRadius=20; //Default 10
+		hiddenSelections[]=
+		{
+			"Camo1",
+			"Camo2",
+			"Camo3"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_ext01_CO.paa",
+			"\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_ext02_CO.paa",
+			"\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_fuel_CO.paa"
+		};
+		class TextureSources
+		{
+			class Standard
+			{
+				displayName="Republic Standard";
+				author="Helix Studios";
+				textures[]=
+				{
+					"\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_ext01_CO.paa",
+			        "\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_ext02_CO.paa",
+			        "\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_fuel_CO.paa"
+				};
+				factions[]=
+				{
+					"HS_GAR_Fac"
+				};
+			};
+		};
+		textureList[]=
+		{
+			"Standard",
+			1
+		};
+    }
+	class HS_GAR_Truck_03_ammo_F: O_Truck_03_transport_F
+    {
+		scope=2;
+		side=1;
+		faction="HS_GAR_Fac";
+        editorSubcategory="HS_CAR";   
+		crew="B_Soldier_F";
+		displayName="Tempest Supply";
+		model="\A3\Soft_F_EPC\Truck_03\Truck_03_box_F.p3d";
+		picture="\A3\Soft_F_EPC\Truck_03\Data\UI\truck_03_box_CA.paa";
+		icon="\A3\Soft_F_EPC\Truck_03\Data\UI\map_Truck_03_box_CA.paa";
+		transportAmmo=1e+012;
+		supplyRadius=10;
+		vehicleClass="Support";
+		hiddenSelections[]=
+		{
+			"Camo1",
+			"Camo2",
+			"Camo3",
+			"Camo4"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_ext01_CO.paa",
+			"\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_ext02_CO.paa",
+			"\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_cargo_CO.paa",
+			"\A3\Structures_F\Data\Metal\Containers\Containers_02_set_CO.paa"
+		};
+		class TextureSources
+		{
+			class Standard
+			{
+				displayName="Republic Standard";
+				author="Helix Studios";
+				textures[]=
+				{
+					"\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_ext01_CO.paa",
+			        "\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_ext02_CO.paa",
+			        "\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_cargo_CO.paa",
+                    "\A3\Structures_F\Data\Metal\Containers\Containers_02_set_CO.paa"                               
+				};
+				factions[]=
+				{
+					"HS_GAR_Fac"
+				};
+			};
+		};
+		textureList[]=
+		{
+			"Standard",
+			1
+		};
+    }
+	class HS_GAR_Truck_03_repair_F: O_Truck_03_transport_F
+    {
+		scope=2;
+		side=1;
+		faction="HS_GAR_Fac";
+        editorSubcategory="HS_CAR";
+		crew="B_Soldier_F";
+		displayName="Tempest Repair";
+		model="\A3\Soft_F_EPC\Truck_03\Truck_03_repair_F.p3d";
+		picture="\A3\Soft_F_EPC\Truck_03\Data\UI\truck_03_ammo_CA.paa";
+		icon="\A3\Soft_F_EPC\Truck_03\Data\UI\map_Truck_03_ammo_CA.paa";
+		vehicleClass="Support";
+		transportRepair=1e+012;
+		supplyRadius=10;
+		hiddenSelections[]=
+		{
+			"Camo1",
+			"Camo2",
+			"Camo3"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_ext01_CO.paa",
+			"\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_ext02_CO.paa",
+			"\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_ammo_CO.paa"
+		};
+		class TextureSources
+		{
+			class Standard
+			{
+				displayName="Republic Standard";
+				author="Helix Studios";
+				textures[]=
+				{
+					"\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_ext01_CO.paa",
+			        "\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_ext02_CO.paa",
+			        "\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_ammo_CO.paa"
+				};
+				factions[]=
+				{
+					"HS_GAR_Fac"
+				};
+			};
+		};
+		textureList[]=
+		{
+			"Standard",
+			1
+		};
+    }
+	class HS_GAR_Truck_03_device_F: O_Truck_03_transport_F
+    {
+		scope=2;
+		side=1;
+		faction="HS_GAR_Fac";
+        editorSubcategory="HS_CAR";   
+		crew="B_Soldier_F";
+		displayName="Tempest Device";
+		model="\A3\Soft_F_EPC\Truck_03\Truck_03_device_F.p3d";
+		picture="\A3\Soft_F_EPC\Truck_03\Data\UI\truck_03_device_CA.paa";
+		icon="\A3\Soft_F_EPC\Truck_03\Data\UI\map_Truck_03_Device_CA.paa";
+		hiddenSelections[]=
+		{
+			"Camo1",
+			"Camo2",
+			"Camo3",
+			"Camo4",
+			"Camo5"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_ext01_CO.paa",
+			"\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_ext02_CO.paa",
+			"\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_cargo_CO.paa",
+			"\A3\Structures_F_EPC\Items\Electronics\Data\The_Device_02_CO.paa",
+			"\A3\Structures_F_EPC\Items\Electronics\Data\The_Device_03_CO.paa"
+		};
+		class TextureSources
+		{
+			class Standard
+			{
+				displayName="Republic Standard";
+				author="Helix Studios";
+				textures[]=
+				{
+					"\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_ext01_CO.paa",
+			        "\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_ext02_CO.paa",
+			        "\HS_Opfor_Veh\HS_Tempest\data\HS_GAR_Truck_03_cargo_CO.paa",
+                    "\A3\Structures_F_EPC\Items\Electronics\Data\The_Device_02_CO.paa",
+					"\A3\Structures_F_EPC\Items\Electronics\Data\The_Device_03_CO.paa"                             
+				};
+				factions[]=
+				{
+					"HS_GAR_Fac"
+				};
+			};
+		};
+		textureList[]=
+		{
+			"Standard",
 			1
 		};
     }
